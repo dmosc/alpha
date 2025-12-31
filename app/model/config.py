f67438c23faea0db955e0758864b151496aaab7b
dmosc <osdavrm@gmail.com>
2025-12-31 14:51:52 -0600

    Adjust config default params to align with input.
    Using multiples of 5 since that's the number of input dimensions we're starting with (input_dims). This will likely enable the exact number of channels for gradients to flow that are dedicated to each feature.

--- app/model/config.py
+++ app/model/config.py
@@ -1,10 +1,14 @@
 class Config:
     def __init__(self):
+        self.epochs = 10
         self.batch_size = 32
         self.seq_len = 30
-        self.input_dims = 6
-        self.d_model = 120
+        self.input_dims = 5
+        self.d_model = 150
         self.dropout = 0.1
-        self.n_heads = 6
+        self.n_heads = 10
         self.d_ff = int(8 / 3) * self.d_model
         self.num_layers = 5
+        self.lr = 1e-3
+        self.betas = (0.9, 0.98)
+        self.max_norm = 1.0
