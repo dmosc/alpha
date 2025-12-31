f23010627ac812436389b9453c774a106f3f578b
dmosc <osdavrm@gmail.com>
2025-12-31 14:50:12 -0600

    Fix PositionalEncoding algorithm.
    `div_term` inside the PositionalEncoding (PE) was applying exp to the raw arange, causing overflow to inf. We have to include the multiplication against the negative log to downscale before exponentiating.

--- app/model/positional_encoding.py
+++ app/model/positional_encoding.py
@@ -10,8 +10,8 @@ class PositionalEncoding(torch.nn.Module):
         self.dropout = torch.nn.Dropout(p=config.dropout)
         positional_encoding = torch.zeros(config.seq_len, config.d_model)
         position = torch.arange(0, config.seq_len).unsqueeze(1)
-        div_term = torch.exp(torch.arange(0, config.d_model, 2)) * \
-            (-math.log(10_000.0) / config.d_model)
+        div_term = torch.exp(torch.arange(0, config.d_model, 2) * \
+            (-math.log(10_000.0) / config.d_model))
         positional_encoding[:, 0::2] = torch.sin(position * div_term)
         positional_encoding[:, 1::2] = torch.cos(position * div_term)
         self.register_buffer('positional_encoding',
