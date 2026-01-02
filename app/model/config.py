4ed42aceb473788ab2c251297a7a54176b2ed422
dmosc <osdavrm@gmail.com>
2026-01-01 23:39:36 -0600

    Update config params.
    * Increased batch size to 32 to give more examples to the model on every evaluation to generalize better.
    * Bumped d_model to 180 to assign 3 dimensions to every element in seq_len (60); hopefully this gives enough room for individual dimensions to learn each of the sequence elements.
    * Setting n_heads to 12 so that each one can own 5 elements from the entire seq_len.
    * Increased num_layers to 10 to double the depth of the network to have more room for longer term relationships.

--- app/model/config.py
+++ app/model/config.py
@@ -7,17 +7,17 @@ class Config:
     def __init__(self, data_dir: Path):
         self.ticker = 'SPY'
         self.epochs = 10
-        self.batch_size = 16
+        self.batch_size = 32
         self.seq_len = 60
         self.training_features = ['Open', 'High', 'Low', 'Close', 'Volume',
                                   'LogReturn']
         self.input_dims = len(self.training_features)
         self.target_idx = self.training_features.index('LogReturn')
-        self.d_model = 150
+        self.d_model = 180
         self.dropout = 0.1
-        self.n_heads = 10
+        self.n_heads = 12
         self.d_ff = int(8 / 3) * self.d_model
-        self.num_layers = 5
+        self.num_layers = 10
         self.lr = 1e-5
         self.betas = (0.9, 0.98)
         self.max_norm = 1.0
