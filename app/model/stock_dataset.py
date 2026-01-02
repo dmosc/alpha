63b138dfea245771a8a898227b08e8c4168f293c
dmosc <osdavrm@gmail.com>
2026-01-01 21:58:25 -0600

    Refactor StockDataset transformation into method.
    Code cleanup.

--- app/model/stock_dataset.py
+++ app/model/stock_dataset.py
@@ -12,16 +12,19 @@ class StockDataset(Dataset):
         self.config = config
         paths = sorted(config.training_data_dir.glob(f"{config.ticker}_*.csv"))
         dataframe = pd.concat([pd.read_csv(p) for p in paths])
-        dataframe['LogReturn'] = np.log(
-            dataframe['Close'] / dataframe['Close'].shift(1))
-        dataframe.loc[0, 'LogReturn'] = 0
-        self.data = torch.tensor(dataframe[config.training_features].values,
-                                 dtype=torch.float)
+        self.data = self._apply_transformations(dataframe)
 
     def __len__(self):
         return len(self.data) - self.config.seq_len
 
-    def __getitem__(self, idx):
+    def __getitem__(self, idx: int):
         input = self.data[idx : idx + self.config.seq_len]
         target = self.data[idx + self.config.seq_len, self.config.target_idx]
         return input, target
+
+    def _apply_transformations(self, dataframe: pd.DataFrame) -> torch.Tensor:
+        dataframe['LogReturn'] = np.log(
+            dataframe['Close'] / dataframe['Close'].shift(1))
+        dataframe.loc[0, 'LogReturn'] = 0
+        return torch.tensor(dataframe[self.config.training_features].values,
+                                 dtype=torch.float)
